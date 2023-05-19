3d0b42936e15e346dd93eb7268d59a297a8da377
Vihanga Wagholkar <vihanga.wagholkar@gmail.com>
2023-05-18 20:08:32 -0700

    AVL Tree with LL and RR Rotation. Also, Inorder function added to check validity.

--- trees/simple bst.cpp
+++ trees/simple bst.cpp
@@ -20,7 +20,12 @@ Node *root=NULL;
 class Tree{
 public:
 	Node *insert(Node *p,int key);
+	Node *Delete(Node *p, int key);
 	void preorder(Node *p);
+	int Height(Node *p);
+
+	Node *Inpre(Node *p);
+	Node *Insucc(Node *p);
 };
 
 Node *Tree:: insert(Node *p,int key){
@@ -51,6 +56,77 @@ void Tree::preorder(Node *p){
 	}
 }
 
+int Tree::Height(Node *p){
+	int x,y;
+	if(p){
+		x=Height(p->lchild);
+		y=Height(p->rchild);
+
+		return x>y?x+1:y+1;
+	}
+
+	else
+		return 0;
+}
+
+Node *Tree::Inpre(Node *p){
+	while(p && p->rchild!=NULL)
+		p=p->rchild;
+
+	return p;
+}
+
+Node *Tree::Insucc(Node *p){
+	while(p && p->lchild!=NULL)
+		p=p->lchild;
+
+	return p;
+}
+
+Node* Tree::Delete(Node *p, int key){
+
+	Node *q;
+
+	if(p==NULL)
+		return NULL;
+
+	if(p->rchild==NULL&&p->lchild==NULL){
+		if(p==root)
+			root=NULL;
+		free(p);
+
+		return NULL;
+	}
+
+	// return p;
+
+	if(key<p->data){
+		p->lchild=Delete(p->lchild,key);
+	}
+
+	else if(key>p->data)
+		p->rchild=Delete(p->rchild,key);
+
+	else{
+		if(Height(p->lchild)>Height(p->rchild)){
+			q=Inpre(p->lchild);
+			p->data=q->data;
+			p->lchild=Delete(p->lchild,q->data);
+
+		}
+		else{
+			q=Insucc(p->rchild);
+			p->data=q->data;
+			p->rchild=Delete(p->rchild,q->data);
+		}
+	}
+
+	return p;
+}
+
+
+
+
 int main(){
 	//Your code here
 	int n,x;
@@ -70,6 +146,17 @@ int main(){
 	// root=insert(root,25);
 
 	cout<<endl<<"Inorder traversal of nodes: ";
+
+	// To check valdity of bst
+	t.preorder(root);
+
+	cout<<endl<<"Enter the element you want to delete: ";
+	cin>>x;
+	t.Delete(root,x);
+
+	cout<<endl<<"Inorder traversal of nodes: ";
+
+	// To check valdity of bst
 	t.preorder(root);
 	return 0;
 }
